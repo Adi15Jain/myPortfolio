00fb67ec5a54e7a79cab80da820e4b1de344124f
Adi Jain <adijain@172-9-10-231.lightspeed.irvnca.sbcglobal.net>
2025-05-11 17:48:22 +0530

    initial commit

--- src/App.jsx
+++ src/App.jsx
@@ -1,5 +1,5 @@
 import LogoSection from "./sections/LogoSection";
-import NavBar from "./components/Navbar";
+import NavBar from "./components/NavBar";
 import FeatureCards from "./sections/FeatureCards";
 import Hero from "./sections/hero";
 import ShowcaseSection from "./sections/ShowcaseSection";
